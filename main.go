4a8c19a1c13cf467d137399dec140c7dded7235f
nattanjunior <fnatanieljunior@gmail.com>
2026-03-10 09:01:59 -0300

    created initial states of the game and the questions

--- main.go
+++ main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+
+type Questions struct {
+	Text string
+	Options []string
+	Answer int
+}
+
+type Game struct {
+	Nome  string
+	Points int 
+	Questions []Questions
+}
+
+
+
 func main() {
 	fmt.Println("Hello, World!")
 }
