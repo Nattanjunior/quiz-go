34d52254bb0c734445a6d326c0c932bdda04709a
nattanjunior <fnatanieljunior@gmail.com>
2026-03-10 10:05:01 -0300

    A function called init was created to retrieve the username from the terminal.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 
@@ -17,9 +19,23 @@ type Game struct {
 	Questions []Questions
 }
 
+func (g *Game) Init() {
+	fmt.Println("Seja bem-vindo ao jogo de perguntas e respostas!")
+	fmt.Println("Qual é o seu nome?")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n') 
 
+	if err != nil {
+		panic("Erro ao ler a string")
+	}
+
+	g.Nome = name
+	fmt.Printf("Vamos ao jogo %s", g.Nome)
+	
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	game := &Game{}
+	game.Init()
 }
 
